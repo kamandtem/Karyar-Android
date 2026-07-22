d02c31ca7d627aa42710a0dd333cb734953aa309
کمند تیم <78971163+kamandtem@users.noreply.github.com>
2026-07-23 02:13:29 +0330

    Update MainActivity.java

--- android/app/src/main/java/com/kamand/karyar/MainActivity.java
+++ android/app/src/main/java/com/kamand/karyar/MainActivity.java
@@ -1,28 +1,5 @@
 package com.kamand.karyar;
 
-import android.widget.Toast;
-
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-
-    private long lastBackPressTime = 0;
-
-    @Override
-    public void onBackPressed() {
-
-        long currentTime = System.currentTimeMillis();
-
-        if (currentTime - lastBackPressTime < 2000) {
-            finish();
-        } else {
-            lastBackPressTime = currentTime;
-
-            Toast.makeText(
-                    this,
-                    "برای خروج دوباره دکمه برگشت را بزنید",
-                    Toast.LENGTH_SHORT
-            ).show();
-        }
-    }
-}
+public class MainActivity extends BridgeActivity {}
